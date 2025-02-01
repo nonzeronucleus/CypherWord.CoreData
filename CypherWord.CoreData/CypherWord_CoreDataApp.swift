24576a0d770237f00d9b833cbd8a02a6c29e9f40
Ian <nonzeronucleus@ianplumb.org>
2025-02-01 07:56:42 +0000

    Initial Commit

---
+++ CypherWord.CoreData/CypherWord_CoreDataApp.swift
@@ -0,0 +1,17 @@
+//
+//  CypherWord_CoreDataApp.swift
+//  CypherWord.CoreData
+//
+//  Created by Ian Plumb on 01/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct CypherWord_CoreDataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CypherWord.CoreDataTests/CypherWord_CoreDataTests.swift
@@ -0,0 +1,17 @@
+//
+//  CypherWord_CoreDataTests.swift
+//  CypherWord.CoreDataTests
+//
+//  Created by Ian Plumb on 01/02/2025.
+//
+
+import Testing
+@testable import CypherWord_CoreData
+
+struct CypherWord_CoreDataTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
